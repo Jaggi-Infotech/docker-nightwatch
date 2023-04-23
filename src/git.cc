62eb5af946004dc55ae94c590cec3a1ca95bec5c
Jaggi Infotech <infotech@jaggi.co>
2023-04-24 01:24:33 +0530

    Improve git repository fetching.
     - Set base data dir to ~/.local/share/nightwatch
     - Give names to repos
     - Do not clone if cloned once before
     - Better status and logging
     - Enable static linking

--- src/git.cc
+++ src/git.cc
@@ -1,13 +1,19 @@
 #include "git.h"
 
+#include <cstdlib>
+#include <filesystem>
+
 #include "absl/log/log.h"
 #include "git2/clone.h"
 #include "git2/errors.h"
 #include "git2/global.h"
 
 namespace nightwatch {
-
 namespace {
+
+const std::filesystem::path GetBaseDataDir() {
+  return std::filesystem::path(std::getenv("HOME")) / ".local/share/nightwatch";
+}
 
 std::string GetGitError() {
   const git_error *error = git_error_last();
@@ -16,32 +22,57 @@ std::string GetGitError() {
   return error->message;
 }
 
+std::string GetNameFromLocation(const std::string_view location) {
+  return std::filesystem::path(location).stem();
+}
+
 } // namespace
 
-GitRepo::GitRepo(const std::string &location) : location_(location) {}
+GitRepo::GitRepo(const std::string_view location)
+    : GitRepo(GetNameFromLocation(location), location) {}
+
+GitRepo::GitRepo(const std::string_view name, const std::string_view location)
+    : name_(name), location_(location) {}
+
+const std::filesystem::path GitRepo::GetRepoFilePath() {
+  return GetBaseDataDir() / name_;
+};
 
 absl::Status GitRepo::Initialize() {
-  int status;
-  status = git_libgit2_init();
-  if (status < 0) {
-    LOG(ERROR) << status << ":" << GetGitError();
+  // TODO: Verify name does not have a / and is not a ., etc.
+
+  // Initialize Git
+  int st;
+  st = git_libgit2_init();
+  if (st < 0) {
+    return absl::UnknownError(GetGitError());
   }
 
-  git_repository *repo;
-  git_clone_options opts = GIT_CLONE_OPTIONS_INIT;
-  status = git_clone(&repo, location_.c_str(), "/tmp/nightwatch_repo", &opts);
-  if (status != 0) {
-    LOG(ERROR) << status << ":" << GetGitError();
+  // Check if the repository has already been cloned
+  st = git_repository_open(&repo_, GetRepoFilePath().string().c_str());
+  if (st < 0) {
+    LOG(INFO) << "Cloning repo from upstream: " << location_;
+
+    // Delete existing data at location
+    std::error_code error_code;
+    std::filesystem::remove_all(GetRepoFilePath(), error_code);
+
+    // Clone the repo
+    git_clone_options opts = GIT_CLONE_OPTIONS_INIT;
+    st = git_clone(&repo_, location_.c_str(),
+                   GetRepoFilePath().string().c_str(), &opts);
+    if (st != 0) {
+      return absl::UnknownError(GetGitError());
+    }
   }
 
-  // TODO: Instead of logging the errors, return an apt status object.
+  return absl::OkStatus();
+}
+
+absl::Status GitRepo::ChooseBranch(const std::string_view branch_name) {
   return {};
 }
 
-absl::Status GitRepo::ChooseBranch(const std::string &branch_name) {
-  return {};
-}
-
-char *GitRepo::GetTarBall() { return nullptr; }
+void GitRepo::GetTarBall() {}
 
 } // namespace nightwatch
--- src/git.h
+++ src/git.h
@@ -1,19 +1,28 @@
+#include <filesystem>
 #include <string>
 
 #include "absl/status/status.h"
+#include "git2/types.h"
 
 namespace nightwatch {
 
 class GitRepo {
 public:
-  GitRepo(const std::string &location);
+  // Constructors
+  explicit GitRepo(const std::string_view location);
+  GitRepo(const std::string_view name, const std::string_view location);
+
+  // Operations
   absl::Status Initialize();
-  absl::Status ChooseBranch(const std::string &branch_name);
-  // TODO: change return type
-  char *GetTarBall();
+  absl::Status ChooseBranch(const std::string_view branch_name);
+  void GetTarBall();
 
 private:
-  std::string location_;
+  const std::filesystem::path GetRepoFilePath();
+
+  const std::string name_; // must be unique
+  const std::string location_;
+  git_repository *repo_ = nullptr;
 };
 
 } // namespace nightwatch
